8c99e792ed2e07927bcb86538e3c994ccc2f8a17
Braden Shepherdson <braden.shepherdson@gmail.com>
2014-12-29 22:17:15 +0000

    Fixed and rearranged. find() now working great!

--- js/foam/dao/DatastoreDAO.js
+++ js/foam/dao/DatastoreDAO.js
@@ -17,6 +17,7 @@ MODEL({
       'int': 'integerValue',
       'Int': 'integerValue',
       'String': 'stringValue',
+      'string': 'stringValue',
       'Boolean': 'booleanValue',
       'float': 'doubleValue',
       'datetime': 'dateTimeValue'
@@ -55,12 +56,13 @@ MODEL({
   name: 'DatastoreSerializationTrait',
   properties: [
     {
-      name: 'datastoreKeyMap',
+      name: 'datastoreKeyMap_',
       lazyFactory: function() {
         var ps = this.model_.properties;
         var map = {};
         for ( var i = 0 ; i < ps.length ; i++ ) {
           var prop = ps[i];
+          console.log(prop.name + ': ' + prop.datastoreKey);
           if ( prop.datastoreKey ) {
             map[prop.datastoreKey] = prop.name;
           }
@@ -78,12 +80,13 @@ MODEL({
       // Ignoring the key for now, iterate the properties object.
       var keys = Object.keys(json.properties);
       for ( var i = 0 ; i < keys.length ; i++ ) {
-        var foamKey = this.datastoreKeyMap[keys[i]];
+        var foamKey = this.datastoreKeyMap_[keys[i]];
         if ( ! foamKey ) {
           console.warn('Unhandled datastore property: ' + keys[i]);
           continue;
         }
-        var prop = this[foamKey.constantize()];
+        console.log('Datastore key: ' + keys[i] + ', foamKey: ' + foamKey + ', constant: ' + foamKey.constantize());
+        var prop = this.model_[foamKey.constantize()];
         if ( ! prop ) {
           console.warn('Failed to find corresponding FOAM property ' + foamKey + ' for datastore property ' + keys[i]);
           continue;
@@ -102,7 +105,7 @@ MODEL({
 
 MODEL({
   name: 'DatastoreStringProperty',
-  extendsModel: 'IntProperty',
+  extendsModel: 'StringProperty',
   traits: ['DatastorePropertyTrait']
 });
 
@@ -118,27 +121,10 @@ MODEL({
   traits: ['DatastorePropertyTrait']
 });
 
-
-// DEBUG: Remove me.
 MODEL({
-  name: 'Activity',
-  traits: ['DatastoreSerializationTrait'],
-  properties: [
-    {
-      model_: 'DatastoreStringProperty',
-      name: 'title',
-      documentation: 'The name of this activity.'
-    },
-    {
-      model_: 'DatastoreBooleanProperty',
-      name: 'isDeleted',
-      defaultValue: false
-    },
-    {
-      model_: 'DatastoreIntProperty',
-      name: 'weight'
-    }
-  ]
+  name: 'DatastoreBooleanProperty',
+  extendsModel: 'BooleanProperty',
+  traits: ['DatastorePropertyTrait']
 });
 
 
@@ -256,11 +242,24 @@ MODEL({
       var keyPath = this.stringToKey(id);
       var req = { keys: [{ path: keyPath }] };
 
-      console.log(require('util').inspect(req, { depth: null }));
       aseq(this.datastore.withClientExecute('lookup', req))(function(res) {
-        // TODO: Implement properly.
-        console.log('Response received', require('util').inspect(res, {depth: null}));
-      });
+        // Response contains "found", "missing" and "deferred" sections.
+        // No idea how to handle "deferred", but "found" is put() and "missing"
+        // is error().
+        if ( res.deferred && res.deferred.length ) {
+          // TODO(braden): Handle "deferred".
+          console.warn('Deferred response to find(). Handle me somehow.');
+        }
+
+        if ( res.found && res.found.length ) {
+          var obj = this.model.create();
+          console.log(res.found[0].entity);
+          obj.fromDatastore(res.found[0].entity);
+          sink && sink.put && sink.put(obj);
+        } else if ( res.missing && res.missing.length ) {
+          sink && sink.error && sink.error('Failed to find ' + id);
+        }
+      }.bind(this));
     },
 
     // Turns a string key into a [{ kind: 'MyEntity', id: '123' }, ...] array.
@@ -278,6 +277,31 @@ MODEL({
     }
   }
 });
+
+
+
+// DEBUG: Remove me.
+MODEL({
+  name: 'Activity',
+  traits: ['DatastoreSerializationTrait'],
+  properties: [
+    {
+      model_: 'DatastoreStringProperty',
+      name: 'title',
+      documentation: 'The name of this activity.'
+    },
+    {
+      model_: 'DatastoreBooleanProperty',
+      name: 'isDeleted',
+      defaultValue: false
+    },
+    {
+      model_: 'DatastoreIntProperty',
+      name: 'weight'
+    }
+  ]
+});
+
 
 
 // Testing support.
